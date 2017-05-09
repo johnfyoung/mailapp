479ba66e34bb4d68b3019f7a4f5c22759034ab8b
John Young <john@codeandcreative.com>
2017-05-09 07:47:54 -0700

    WIP; added a barebones README

---
+++ application/controllers/api/1/Mailservice_controller.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Mailservice_controller.php
+ *
+ * @package mailapp
+ * @author johny
+ * @copyright Copyright (c) 2016, codeandcreative
+ * Date: 10/16/16
+ * Time: 10:31 AM
+ */
+
+class Mailservice_controller extends REST_Controller {
+	public function __construct() {
+
+	}
+
+
+}
